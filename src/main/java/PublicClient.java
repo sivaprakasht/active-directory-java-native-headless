74aeacfb7c763399472ecad04bd0399a8f6f6317
sthiyag <sivaprakash_thiyagarajan@trimble.com>
2016-09-26 16:37:58 +0530

    Changes as per my working Active directory.

--- src/main/java/PublicClient.java
+++ src/main/java/PublicClient.java
@@ -8,27 +8,29 @@ import javax.naming.ServiceUnavailableException;
 
 import com.microsoft.aad.adal4j.AuthenticationContext;
 import com.microsoft.aad.adal4j.AuthenticationResult;
+import com.microsoft.aad.adal4j.ClientCredential;
 
 public class PublicClient {
 
-    private final static String AUTHORITY = "https://login.microsoftonline.com/common/";
-    private final static String CLIENT_ID = "2a4da06c-ff07-410d-af8a-542a512f5092";
+    private final static String AUTHORITY = "https://login.microsoftonline.com/common";
+    private final static String CLIENT_ID = "c881b25f-e1c2-4321-baf5-81cf9ee40579";
+
+    private final static String SECRET = "9kDNZzKR724tnJ9V8/+Rt/cXQ0w+PuXkXy2vxGhLDoU=";
 
     public static void main(String args[]) throws Exception {
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(
-                System.in))) {
-            System.out.print("Enter username: ");
-            String username = br.readLine();
-            System.out.print("Enter password: ");
-            String password = br.readLine();
 
-            AuthenticationResult result = getAccessTokenFromUserCredentials(
-                    username, password);
-            System.out.println("Access Token - " + result.getAccessToken());
-            System.out.println("Refresh Token - " + result.getRefreshToken());
-            System.out.println("ID Token - " + result.getIdToken());
-        }
+
+        String username = "raja@chrischrismc.onmicrosoft.com";
+        String password = "ORK33jfk!";
+        System.out.print("Fetching Token . . . . . . ");
+
+        AuthenticationResult result = getAccessTokenFromUserCredentials(
+                username, password);
+        System.out.println("Access Token - " + result.getAccessToken());
+        System.out.println("Refresh Token - " + result.getRefreshToken());
+        System.out.println("ID Token - " + result.getIdToken());
+
     }
 
     private static AuthenticationResult getAccessTokenFromUserCredentials(
@@ -36,15 +38,16 @@ public class PublicClient {
         AuthenticationContext context = null;
         AuthenticationResult result = null;
         ExecutorService service = null;
+        ClientCredential creds = new ClientCredential(CLIENT_ID,SECRET);
+
         try {
             service = Executors.newFixedThreadPool(1);
             context = new AuthenticationContext(AUTHORITY, false, service);
-            Future<AuthenticationResult> future = context.acquireToken(
-                    "https://graph.windows.net", CLIENT_ID, username, password,
-                    null);
+            Future<AuthenticationResult> future = context.acquireToken("https://graph.windows.net",creds ,null);
+            //Future<AuthenticationResult> future = context.acquireToken("https://graph.windows.net",CLIENT_ID,username,password ,null);
             result = future.get();
         } finally {
-            service.shutdown();
+            // service.shutdown();
         }
 
         if (result == null) {
